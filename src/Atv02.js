7ad91b1f82ec0656363a2bb97550efb3f8bb834f
EdyFS <157139859+EdyFS@users.noreply.github.com>
2024-11-23 19:58:04 -0300

    Contador
    Contador

--- src/Atv02.js
+++ src/Atv02.js
@@ -1,24 +1,35 @@
 import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
+import './Atv02.css';
 
 function Atv02() {
 
     const [homens, setHomens] = useState(0);
     const [mulheres, setMulheres] = useState(0);
     const [total, setTotal] = useState(0);
+    const imghomem = {
+      imageUrl :'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.flaticon.com%2Fbr%2Ficone-gratis%2Fhomem_3439472&psig=AOvVaw2Dpfxc5Fi3iP6Aa9yLGsMu&ust=1732487161323000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCJDXkbbG84kDFQAAAAAdAAAAABAE',
+    }
+    const imgmulher = {
+      imageUrl :'https://www.google.com/imgres?q=mulher%20icone&imgurl=https%3A%2F%2Fcdn-icons-png.flaticon.com%2F512%2F103%2F103277.png&imgrefurl=https%3A%2F%2Fwww.flaticon.com%2Fbr%2Ficone-gratis%2Fmulher_103277&docid=hAjay27vzL79kM&tbnid=VPVVaaPMrCcPdM&vet=12ahUKEwjG9Kb0v_OJAxX4gWEGHRjkOigQM3oECBcQAA..i&w=512&h=512&hcb=2&ved=2ahUKEwjG9Kb0v_OJAxX4gWEGHRjkOigQM3oECBcQAA',
+    }
 
     function addHomem(){
       setHomens(homens + 1);
+      setTotal(total + 1);
     }
 
     function addMulher(){
       setMulheres(mulheres + 1);
+      setTotal(total + 1);
     }
     function rmHomem(){
       setHomens(homens - 1);
+      setTotal(total - 1);
     }
     function rmMulher(){
-      setMulheres(mulheres + 1);
+      setMulheres(mulheres - 1);
+      setTotal(total - 1);
     }
     function addTotal(){
       total = homens + mulheres;
@@ -27,12 +38,40 @@ function Atv02() {
 
     }
     function limpar(){
-
+      setHomens(0);
+      setMulheres(0);
+      setTotal(0);
     }
     return (
       <>
-        <h2>Atividade 02</h2>
-
+        <div className="contador">
+          <h2>Atividade 02</h2>
+          <div className="topo">
+            <div className="total">
+              <h3>Total</h3>
+              <div className="valor">{total}</div>
+            </div>
+            <div>
+              <button onClick={limpar}>Limpar</button>
+            </div>
+          </div>
+          <div className="pessoas">
+            <div className="baixo">
+              <img src="imghomem.imageUrl" alt="Homem"></img>
+              <button onClick={addHomem} className="mais">+</button>
+              <button onClick={rmHomem} className="menos">-</button>
+              <h3>Homens</h3>
+              <div className="valor">{homens}</div>
+            </div>
+            <div className="baixo">
+              <img src="imgmulher.imageUrl" alt="Mulher"></img>
+              <button onClick={addMulher} className="mais">+</button>
+              <button onClick={rmMulher} className="menos">-</button>
+              <h3>Mulheres</h3>
+              <div className="valor">{mulheres}</div>
+            </div>
+          </div>
+        </div>
         <Link to="/">Voltar</Link>
       </>
      
