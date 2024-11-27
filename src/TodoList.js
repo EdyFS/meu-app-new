2aee18eb9e71562f69964f86c84938502beebd3a
EdyFS <157139859+EdyFS@users.noreply.github.com>
2024-11-26 21:13:17 -0300

    Atividade 3

--- src/TodoList.js
+++ src/TodoList.js
@@ -19,10 +19,10 @@ function formatDate(date){
 export default function TodoList(){
     return(
         <>
-        <h1>Mendeleev</h1>
+        {/*<h1>Mendeleev</h1>
         <img src="https://upload.wikimedia.org/wikipedia/commons/5/53/%EB%93%9C%EB%AF%B8%ED%8A%B8%EB%A6%AC_%EB%A9%98%EB%8D%B8%EB%A0%88%EC%98%88%ED%94%84.jpg"
         alt="Mendeleev"
-        className="photo"/>
+        className="photo"/>*/}
 
         <h1>To Do List for {formatDate(today)}</h1>
         <div style={person.theme}>
